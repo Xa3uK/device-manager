9d32d8c172ff3a38c90800586fe47d7c29160608
Koval <hazardsales@gmail.com>
2026-03-28 10:49:20 +0100

    added additional crud endpoints to controller

--- src/main/java/com/koval/devicemanager/api/controller/DeviceController.java
+++ src/main/java/com/koval/devicemanager/api/controller/DeviceController.java
@@ -1,11 +1,15 @@
 package com.koval.devicemanager.api.controller;
 
 import com.koval.devicemanager.api.dto.request.CreateDeviceRequest;
-import com.koval.devicemanager.api.dto.response.CreateDeviceResponse;
+import com.koval.devicemanager.api.dto.request.UpdateDeviceRequest;
+import com.koval.devicemanager.api.dto.response.DeviceResponse;
+import com.koval.devicemanager.domain.model.DeviceState;
 import com.koval.devicemanager.domain.service.DeviceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/devices")
@@ -16,7 +20,34 @@ public class DeviceController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CreateDeviceResponse create(@RequestBody CreateDeviceRequest request) {
-        return new CreateDeviceResponse(deviceService.create(request.getName(), request.getBrand()));
+    public DeviceResponse create(@RequestBody CreateDeviceRequest request) {
+        return new DeviceResponse(deviceService.create(request.getName(), request.getBrand()));
+    }
+
+    @PatchMapping("/{id}")
+    public DeviceResponse update(@PathVariable Long id, @RequestBody UpdateDeviceRequest request) {
+        return new DeviceResponse(
+                deviceService.update(id, request.getName(), request.getBrand(), request.getState())
+        );
+    }
+
+    @GetMapping("/{id}")
+    public DeviceResponse getById(@PathVariable Long id) {
+        return new DeviceResponse(deviceService.getById(id));
+    }
+
+    @GetMapping
+    public List<DeviceResponse> getAll() {
+        return deviceService.getAll().stream().map(DeviceResponse::new).toList();
+    }
+
+    @GetMapping("/brand/{brand}")
+    public List<DeviceResponse> getAllByBrand(@PathVariable String brand) {
+        return deviceService.getAllByBrand(brand).stream().map(DeviceResponse::new).toList();
+    }
+
+    @GetMapping("/state/{state}")
+    public List<DeviceResponse> getAllByState(@PathVariable DeviceState state) {
+        return deviceService.getAllByState(state).stream().map(DeviceResponse::new).toList();
     }
 }
