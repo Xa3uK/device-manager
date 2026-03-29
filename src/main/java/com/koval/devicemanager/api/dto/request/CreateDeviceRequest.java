cc3feb511e73c6997eaaf975f03bbbe6605213e0
Koval <hazardsales@gmail.com>
2026-03-29 14:32:02 +0200

    added inputs validation

--- src/main/java/com/koval/devicemanager/api/dto/request/CreateDeviceRequest.java
+++ src/main/java/com/koval/devicemanager/api/dto/request/CreateDeviceRequest.java
@@ -1,9 +1,17 @@
 package com.koval.devicemanager.api.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class CreateDeviceRequest {
+
+    @NotBlank
+    @Size(max = 255)
     private String name;
+
+    @NotBlank
+    @Size(max = 255)
     private String brand;
 }
--- src/main/java/com/koval/devicemanager/api/dto/request/UpdateDeviceRequest.java
+++ src/main/java/com/koval/devicemanager/api/dto/request/UpdateDeviceRequest.java
@@ -3,15 +3,18 @@ package com.koval.devicemanager.api.dto.request;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.annotation.Nulls;
 import com.koval.devicemanager.domain.model.DeviceState;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UpdateDeviceRequest {
 
     @JsonSetter(nulls = Nulls.FAIL)
+    @Size(min = 1, max = 255)
     private String name;
 
     @JsonSetter(nulls = Nulls.FAIL)
+    @Size(min = 1, max = 255)
     private String brand;
 
     @JsonSetter(nulls = Nulls.FAIL)
