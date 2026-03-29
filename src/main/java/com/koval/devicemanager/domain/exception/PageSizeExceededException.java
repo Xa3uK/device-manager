4aef3cb506d6d7ad27ab1e94510440e30e363b4a
Koval <hazardsales@gmail.com>
2026-03-29 14:33:33 +0200

    removed unused exception

--- src/main/java/com/koval/devicemanager/domain/exception/PageSizeExceededException.java
+++
@@ -1,8 +0,0 @@
-package com.koval.devicemanager.domain.exception;
-
-public class PageSizeExceededException extends RuntimeException {
-
-    public PageSizeExceededException(int requested, int max) {
-        super(String.format("Requested page size %d exceeds the maximum allowed size of %d", requested, max));
-    }
-}
